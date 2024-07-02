b485960c66beccb29430144e837caa5cee423ce2
WestSopho <153777405+WestSopho@users.noreply.github.com>
2024-07-02 16:28:49 -0500

    escape with &apos and update README

--- app/ui/block-2.jsx
+++ app/ui/block-2.jsx
@@ -16,9 +16,9 @@ export default function Block2(){
                     className='max-w-[564px]'
                 />
             </div>
-            <div class='p-5 sm:w-1/2 space-y-7'>
+            <div className='p-5 sm:w-1/2 space-y-7'>
                 <p className='text-heading-M text-gunmetal sm:text-heading-L'>What your BMI result means</p>
-                <p className='text-body-M text-dark-electric-blue'>A BMI range of 18.5 to 24.9 is considered a 'healthy weight.' Maintaining a healthy weight may lower your chances of experiencing health issues later on, such as obesity and type 2 diabetes. Aim for a nutritious diet with reduced fat and sugar content, incorporating ample fruits and vegetables. Additionally, strive for regular physical activity, ideally about 30 minutes daily for five days a week.</p>
+                <p className='text-body-M text-dark-electric-blue'>A BMI range of 18.5 to 24.9 is considered a &apos;healthy weight.&apos; Maintaining a healthy weight may lower your chances of experiencing health issues later on, such as obesity and type 2 diabetes. Aim for a nutritious diet with reduced fat and sugar content, incorporating ample fruits and vegetables. Additionally, strive for regular physical activity, ideally about 30 minutes daily for five days a week.</p>
             </div>
       </div>
     );
