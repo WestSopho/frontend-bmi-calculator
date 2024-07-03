1782fe002486b4d4fa0a6ae2fecc9b8c6c0e343f
WestSopho <153777405+WestSopho@users.noreply.github.com>
2024-07-03 11:30:12 -0500

    fix grid

--- app/ui/block-4.jsx
+++ app/ui/block-4.jsx
@@ -27,10 +27,8 @@ export default function Block4(){
             {/* Grid for Desktop Devices */}
             <div className="hidden lg:block">
                 <div className='p-20 space-y-7'>
-                    {/* First Row */}
-                    <div className='grid grid-rows-1 grid-cols-2 justify-items-center'>
-                        {/* Title and Descrioption */}
-                        <div className='text-start space-y-7 relative'>
+                    <div className='flex'>
+                        <div className='text-start space-y-7 relative w-1/2'>
                             <p className='text-gunmetal text-heading-L'>Limitations of BMI</p>
                             <p className='text-body-M text-dark-electric-blue'>Although BMI is often a practical indicator of healthy weight, it is not suited for every person. Specific groups should carefully consider their BMI outcomes, and in certain cases, the measurement may not be beneficial to use.</p>
                             <Image 
@@ -41,17 +39,24 @@ export default function Block4(){
                                 className='absolute left-1/4'
                             />
                         </div>
-                        <ItemBox src='/icon-gender.svg' title='Gender' description="The development and body fat composition of girls and boys vary with age. Consequently, a child's age and gender are considered when evaluating their BMI."/>
-                    </div>
-                    {/* Second Row */}
-                    <div className='flex justify-end gap-7'>
-                        <ItemBox src='/icon-age.svg' title='Age' description="In aging individuals, increased body fat and muscle loss may cause BMI to underestimate body fat content.."/>
-                        <ItemBox src='/icon-muscle.svg' title='Muscle' description="BMI may misclassify muscular individuals as overweight or obese, as it doesn't differentiate muscle from fat."/>
-                    </div>
-                    {/* Third Row */}
-                    <div className='flex justify-center gap-7'>
-                        <ItemBox src='/icon-pregnancy.svg' title='Pregnancy' description="Expectant mothers experience weight gain due to their growing baby. Maintaining a healthy pre-pregnancy BMI is advisable to minimise health risks for both mother and child."/>
-                        <ItemBox src='/icon-race.svg' title='Race' description="Certain health concerns may affect individuals of some Black and Asian origins at lower BMIs than others. To learn more, it is advised to discuss this with your GP or practice nurse."/>
+
+                        <div className='flex flex-col w-1/2 gap-10'>
+                            {/* First Row */}
+                            <div className='flex justify-center '>
+                                <ItemBox src='/icon-gender.svg' title='Gender' description="The development and body fat composition of girls and boys vary with age. Consequently, a child's age and gender are considered when evaluating their BMI."/>
+                            </div>
+                            {/* Second Row */}
+                            <div className='-ms-[200px] flex justify-center gap-7'>
+                                <ItemBox src='/icon-age.svg' title='Age' description="In aging individuals, increased body fat and muscle loss may cause BMI to underestimate body fat content.."/>
+                                <ItemBox src='/icon-muscle.svg' title='Muscle' description="BMI may misclassify muscular individuals as overweight or obese, as it doesn't differentiate muscle from fat."/>
+                            </div>
+                            {/* Third Row */}
+                            <div className='-ms-[600px] flex justify-center gap-7'>
+                                <ItemBox src='/icon-pregnancy.svg' title='Pregnancy' description="Expectant mothers experience weight gain due to their growing baby. Maintaining a healthy pre-pregnancy BMI is advisable to minimise health risks for both mother and child."/>
+                                <ItemBox src='/icon-race.svg' title='Race' description="Certain health concerns may affect individuals of some Black and Asian origins at lower BMIs than others. To learn more, it is advised to discuss this with your GP or practice nurse."/>
+                            </div>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
